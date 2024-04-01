ab80c45fd1d9ca42e7aaa740aaa38955449da8ae
Vivek Kumar Mishra <155726934+thevivek-mishra@users.noreply.github.com>
2024-04-01 22:19:38 +0530

    Time: 51 ms (52.89%), Space: 49 MB (16.02%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,18 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length === 0)
+    {
+        return -1
+    }else{
+        let lastElement = this[this.length-1]
+        return lastElement
+    }
+
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
